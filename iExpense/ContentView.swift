a7110d63e6413c4074863dcc4b632306fc735000
Khalid Kamil <khalidkamil28@gmail.com>
2023-08-23 11:16:57 +0100

    Build a list we can delete from

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,18 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    @StateObject var expenses = Expenses()
 
     var body: some View {
         NavigationStack {
-            VStack {
-                Button("Tap count: \(tapCount)") {
-                    tapCount += 1
-                    UserDefaults.standard.set(self.tapCount, forKey: "Tap")
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
                 }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button {
+                    let expense = ExpenseItem(name: "Test",
+                                              type: "Personal",
+                                              amount: 5)
+                    expenses.items.append(expense)
+                } label: {
+                    Image(systemName: "plus")
+                }
+
             }
         }
     }
+
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,8 @@
+
+import Foundation
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
